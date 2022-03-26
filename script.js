d3a732192fd0a9e4cc12748ef0160089afa1021f
ichbingrg <65743692+ichbingrg@users.noreply.github.com>
2022-03-26 15:02:14 +0100

    commit on 'main'

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ timeline
     .to(".rock",3,{y:-500})
     .to(".girl",3,{y:-200},"-=3")
     .fromTo(".bg1",{y:-50},{y:0, duration: 3},"-=3")
-    .to(".content",3, {top: "0%"}, "-=3");
+    .to(".content",3, {top: "0%"}, "-=3")
+    .fromTo(".content-images",{opacity:0},{opacity:1, duration : 2},"-=1")
+    .fromTo(".text",{opacity:0},{opacity:1, duration : 1}, "-=1");
 
 let scene = new ScrollMagic.Scene({
     triggerElement: "section",
-    duration: "100%",
+    duration: "300%",
     triggerHook: 0,
 })
     .setTween(timeline)
